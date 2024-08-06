bbe421a8a1e14634545e41bd8039a6ffae7c167a
AKAlSS <43563907+AKAlSS@users.noreply.github.com>
2024-08-06 00:13:07 -0400

    s

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -187,7 +187,6 @@ const ProjectShowcaseSlider = () => {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ duration: 0.5 }}
-            style={{ backgroundImage: `url(${currentProject.image})` }}
             onClick={() => setIsExpanded(!isExpanded)}
           >
             <motion.div 
